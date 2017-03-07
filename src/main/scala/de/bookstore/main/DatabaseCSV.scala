b5386877e84ed9ce76de9e815ed5a44275c62020
Johannes Dienst <johannesdienst@t-online.de>
2017-03-07 20:37:54 +0100

    Exception handling more examples; Example for Either

--- src/main/scala/de/bookstore/main/DatabaseCSV.scala
+++ src/main/scala/de/bookstore/main/DatabaseCSV.scala
@@ -4,7 +4,9 @@ import scala.collection.mutable.ListBuffer
 import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
+import scala.util.control.NonFatal
 
+// TODO: Option, Try
 class DatabaseSQL(dbPath: String = "src/main/resources/database.csv") extends Database {
 
   val books: ListBuffer[Book] = readFromFile()
@@ -16,7 +18,13 @@ class DatabaseSQL(dbPath: String = "src/main/resources/database.csv") extends Da
         println(line)
       }
     } catch {
-      case e:IOException => e.printStackTrace()
+      case e: IOException => e.printStackTrace()
+      
+      // DO NOT USE THIS: Catches JVM errors too!
+      //case e: Throwable => e.printStackTrace()
+
+      // Use this instead to catch everything none fatal
+      case NonFatal(e) => e.printStackTrace()
     } finally {
       reader.close()
     }
@@ -30,11 +38,27 @@ class DatabaseSQL(dbPath: String = "src/main/resources/database.csv") extends Da
   }
 
   def update(book: Book) = {
+
+    /*
+     *  We can do better than this!
+     *
     if (!Book.validateISBN10(book.isbn10)) {
       throw new IllegalArgumentException("Invalid ISBN10");
     }
+    */
+    
+    def validate(book: Book): Either[String, Book] = {
+      if (Book.validateISBN10(book.isbn10)) {
+        Right(book)
+      } else {
+        Left("Book has invalid ISBN10: " + book)
+      }
+    }
 
-    this.books += book
+    validate(book).fold(
+      error => println(error),
+      success => { this.books += book }
+    )
   }
 
   def findBooks(title: String, bookType: String): List[Book] = {
